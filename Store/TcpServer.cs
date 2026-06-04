621a51521f826d3a012e87f7202d79cc304e0be1
Anthony Savchenko <anthony.savchenko@gmail.com>
2026-06-04 23:59:18 +0300

    Добавил TcpServer

--- Store/TcpServer.cs
+++ Store/TcpServer.cs
@@ -1,11 +1,46 @@
+using System.Net;
 using System.Net.Sockets;
 
 namespace Store;
 
 public class TcpServer()
 {
-  public void StartAsync()
+  public async Task StartAsync()
   {
-    var c = new Socket("", ProtocolType.IP);
+    var ipAddress = IPAddress.Any;
+    var port = 8080;
+    var cancelationToken = new CancellationToken();
+
+    try
+    {
+      using var serverSoket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+      serverSoket.Bind(new IPEndPoint(ipAddress, port));
+      serverSoket.Listen();
+
+      while (true)
+      {
+        try
+        {
+          var clientSocket = await serverSoket.AcceptAsync(cancelationToken);
+
+          _ = ProcessClientAsync(clientSocket);
+        }
+        catch (Exception e)
+        {
+          Console.WriteLine(e);
+          break;
+        }
+      }
+    }
+    catch (SocketException e)
+    {
+      Console.WriteLine(e);
+    }
+  }
+
+  private async Task ProcessClientAsync(Socket clientSocket)
+  {
+
   }
 }
