66661a6b95b709a791d48884d007781b7c4eb5f2
Anthony Savchenko <anthony.savchenko@gmail.com>
2026-06-13 16:04:30 +0300

    Улучшил тесты сервера

--- Store.Tests/TcpServerTests.cs
+++ Store.Tests/TcpServerTests.cs
@@ -7,53 +7,76 @@ namespace Store.Tests;
 public class TcpServerTests
 {
   [Fact]
-  public async Task CorrectSetRequest()
+  public async Task CorrectSet()
   {
-    using var stringWriter = new StringWriter();
+    var lines = await SendFromClentToServerAndGetConsoleOutputAsLines("SET user:1 data");
+
+    Assert.Contains("Server 127.0.0.1:8080. Started", lines[0]);
+    Assert.Contains("Client message min bytes for ArrayPool: 64", lines[1]);
+    Assert.Contains("Client 127.0.0.1", lines[2]); Assert.Contains("Connected", lines[2]);
+    Assert.Contains("Client 127.0.0.1", lines[3]); Assert.Contains("Received Command: SET", lines[3]);
+    Assert.Contains("Client 127.0.0.1", lines[4]); Assert.Contains("Received Key: user:1", lines[4]);
+    Assert.Contains("Client 127.0.0.1", lines[5]); Assert.Contains("Received Value: data", lines[5]);
+    Assert.Contains("Client 127.0.0.1", lines[6]); Assert.Contains("Disconnected", lines[6]);
+    Assert.Contains("Server 127.0.0.1:8080. Closed", lines[7]);
+  }
+
+  private static async Task<string[]> SendFromClentToServerAndGetConsoleOutputAsLines(string message)
+  {
+    using var stringWriterOutput = new StringWriter();
     var originalOutput = Console.Out;
-    Console.SetOut(stringWriter);
+
+    Console.SetOut(stringWriterOutput);
 
     try
     {
-      var ipAddress = IPAddress.Parse("127.0.0.1");
-      var port = 8080;
-      var clientMessageMinBytes = 64;
-      var server = new TcpServer(ipAddress, port, clientMessageMinBytes);
-
-      using var cancellationTokenSource = new CancellationTokenSource();
-
-      var serverListeningTask = server.StartAsync(cancellationTokenSource.Token);
-
-      await Task.Run(async () =>
-        {
-          using var client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
-          await client.ConnectAsync(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8080), cancellationTokenSource.Token);
-          await client.SendAsync(Encoding.Unicode.GetBytes("SET user:1 data"), SocketFlags.None, cancellationTokenSource.Token);
-          await client.DisconnectAsync(reuseSocket: false, cancellationTokenSource.Token);
-          client.Shutdown(SocketShutdown.Both);
-          client.Close();
-        },
-        cancellationTokenSource.Token
-      );
-
-      await Task.Delay(500, cancellationTokenSource.Token);
-
-      await cancellationTokenSource.CancelAsync();
-      await serverListeningTask;
-
-      var output = stringWriter.ToString();
-      Assert.Contains("Server 127.0.0.1:8080. Started", output);
-      Assert.Contains("Client message min bytes for ArrayPool: 64", output);
-      Assert.Contains("Client 127.0.0.1:8080. Connected", output);
-      Assert.Contains("Client 127.0.0.1:8080. Buffer: SET user:1 data", output);
-      Assert.Contains("Client 127.0.0.1:8080. Received Command: SET", output);
-      Assert.Contains("Client 127.0.0.1:8080. Received Key: user:1", output);
-      Assert.Contains("Client 127.0.0.1:8080. Received Value: data", output);
-      Assert.Contains("Server 127.0.0.1:8080. Closed", output);
+      await Task.Run(() => SendFromClientToServer(message));
     }
     finally
     {
       Console.SetOut(originalOutput);
     }
+
+    var output = stringWriterOutput.ToString();
+    var lines = output.Split(Environment.NewLine);
+
+    return lines;
+  }
+
+  private static async Task SendFromClientToServer(string message)
+  {
+    var ipAddress = IPAddress.Parse("127.0.0.1");
+    var port = 8080;
+    var clientMessageMinBytes = 64;
+    var server = new TcpServer(ipAddress, port, clientMessageMinBytes);
+
+    using var cancellationTokenSource = new CancellationTokenSource();
+    var cancellationToken = cancellationTokenSource.Token;
+
+    var serverListeningTask = server.StartAsync(cancellationToken);
+
+    var serverEndPoint = new IPEndPoint(ipAddress, port);
+
+    await SendFromClient(message, serverEndPoint, cancellationToken);
+
+    // Ждем перед завершением работы сервера, чтобы он успевал обработать данные клиента
+    await Task.Delay(3000);
+
+    await cancellationTokenSource.CancelAsync();
+
+    // Ждем перед завершением программы, чтобы сервер корректно обработал завершение работы
+    await serverListeningTask;
+  }
+
+  private static async Task SendFromClient(string message, EndPoint serverEndPoint, CancellationToken cancellationToken)
+  {
+    using var client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+
+    await client.ConnectAsync(serverEndPoint, cancellationToken);
+    await client.SendAsync(Encoding.Unicode.GetBytes(message), SocketFlags.None, cancellationToken);
+    await client.DisconnectAsync(reuseSocket: false, cancellationToken);
+
+    client.Shutdown(SocketShutdown.Both);
+    client.Close();
   }
 }
--- Store/TcpServer.cs
+++ Store/TcpServer.cs
@@ -6,8 +6,9 @@ using Store.Parser;
 
 namespace Store;
 
-// TODO: перенести serverSocket в поле класса и вынести ServerSocketInit
+// TODO: перенести serverSocket в поле класса, реализовать IDisposabel и вынести ServerSocketInit
 // TODO: сделать возможность выключать запись в консоль
+// TODO: передвавать в сообщениях размер данных, чтобы ограничивать их в буфере при чтении
 
 public class TcpServer(IPAddress ipAddress, int port, int clientMessageMinBytes)
 {
@@ -29,7 +30,7 @@ public class TcpServer(IPAddress ipAddress, int port, int clientMessageMinBytes)
 
       await WaitAndProcessClientsAsync(serverSocket, cancellationToken);
     }
-    catch (Exception e) when (e is not OperationCanceledException)
+    catch (Exception e)
     {
       Console.WriteLine($"Server {_endPoint}. Exception occured: {e}");
     }
@@ -39,11 +40,11 @@ public class TcpServer(IPAddress ipAddress, int port, int clientMessageMinBytes)
   {
     try
     {
-      while (true)
+      while (!cancellationToken.IsCancellationRequested)
       {
         var clientSocket = await serverSocket.AcceptAsync(cancellationToken);
 
-        Console.WriteLine($"Client {clientSocket.LocalEndPoint}. Connected");
+        Console.WriteLine($"Client {clientSocket.RemoteEndPoint}. Connected");
 
         _ = ProcessClientAsync(clientSocket, cancellationToken);
       }
@@ -62,41 +63,55 @@ public class TcpServer(IPAddress ipAddress, int port, int clientMessageMinBytes)
     {
       try
       {
-        while (true)
+        while (!cancellationToken.IsCancellationRequested)
         {
-          var buffer = ArrayPool<byte>.Shared.Rent(_clientMessageMinBytes);
-          var bytesReceived = await clientSocket.ReceiveAsync(buffer, SocketFlags.None, cancellationToken);
-
-          if (bytesReceived != 0)
-          {
-            var request = CommandParser.ParseBytes(buffer);
-
-            Console.WriteLine($"Client {clientSocket.LocalEndPoint}. Buffer: {Encoding.Unicode.GetString(buffer)}");
-            Console.WriteLine($"Client {clientSocket.LocalEndPoint}. Received Command: {Encoding.Unicode.GetString(request.Command)}");
-            Console.WriteLine($"Client {clientSocket.LocalEndPoint}. Received Key: {Encoding.Unicode.GetString(request.Key)}");
-            Console.WriteLine($"Client {clientSocket.LocalEndPoint}. Received Value: {Encoding.Unicode.GetString(request.Value)}");
-          }
-
-          ArrayPool<byte>.Shared.Return(buffer);
+          var bytesReceived = await WaitAndProcessClientMessageAsync(clientSocket, cancellationToken);
 
           if (bytesReceived == 0) break;
         }
       }
       catch (Exception e) when (e is not OperationCanceledException)
       {
-        Console.WriteLine($"Client {clientSocket.LocalEndPoint}. Exception occured {e}");
+        Console.WriteLine($"Client {clientSocket.RemoteEndPoint}. Exception occured {e}");
       }
       finally
       {
+        var clientEndPoint = clientSocket.RemoteEndPoint;
+
         clientSocket.Shutdown(SocketShutdown.Both);
         clientSocket.Close();
-        Console.WriteLine($"Client {clientSocket.LocalEndPoint}. Disconnected");
+        Console.WriteLine($"Client {clientEndPoint}. Disconnected");
       }
     }
   }
 
-  private async Task<ReadOnlyMemory<byte>> ReadDataFromSocket(TcpClient client)
+  private async Task<int> WaitAndProcessClientMessageAsync(Socket clientSocket, CancellationToken cancellationToken = default)
   {
-    return new ReadOnlyMemory<byte>();
+    var buffer = ArrayPool<byte>.Shared.Rent(_clientMessageMinBytes);
+
+    try
+    {
+      var bytesReceived = await clientSocket.ReceiveAsync(buffer, SocketFlags.None, cancellationToken);
+
+      if (bytesReceived != 0)
+      {
+        ProcessClientMessage(buffer, clientSocket.RemoteEndPoint);
+      }
+
+      return bytesReceived;
+    }
+    finally
+    {
+      ArrayPool<byte>.Shared.Return(buffer);
+    }
+  }
+
+  private static void ProcessClientMessage(byte[] message, EndPoint? clientEndPoint)
+  {
+    var request = CommandParser.ParseBytes(message);
+
+    Console.WriteLine($"Client {clientEndPoint}. Received Command: {Encoding.Unicode.GetString(request.Command)}");
+    Console.WriteLine($"Client {clientEndPoint}. Received Key: {Encoding.Unicode.GetString(request.Key)}");
+    Console.WriteLine($"Client {clientEndPoint}. Received Value: {Encoding.Unicode.GetString(request.Value)}");
   }
 }
